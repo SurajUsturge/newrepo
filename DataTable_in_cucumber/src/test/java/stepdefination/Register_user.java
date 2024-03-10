3a23cfd96ed859cbf3ca3d0c27ff46ed1400ae9c
suraj <suraj.u9903@gmail.com>
2024-03-11 00:48:17 +0530

    commite stepdef in sd245 new

--- DataTable_in_cucumber/src/test/java/stepdefination/Register_user.java
+++ DataTable_in_cucumber/src/test/java/stepdefination/Register_user.java
@@ -25,7 +25,6 @@ public class Register_user
 		driver.get("https://www.amazon.in/ap/register?_encoding=UTF8&openid.assoc_handle=inflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.in%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dnav_newcust");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-	
 	}
 	
 
@@ -39,6 +38,7 @@ public class Register_user
 		driver.findElement(By.xpath("//*[@type='password']")).sendKeys(data.get(3).get(1));
 	}
 
+//	author name
 	@Then("user registered successfull")
 	public void user_registered_successfull() 
 	{
